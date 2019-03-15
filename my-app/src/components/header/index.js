5a4f8a6ca1f5b3a9fb26a1ca47501acd7bf5dbdd
Sara <sarapanoffminerva@yahoo.com>
2019-03-14 20:03:53 -0400

    push changes
    Changes and updates to nav & header files

--- my-app/src/components/header/index.js
+++ my-app/src/components/header/index.js
@@ -5,11 +5,20 @@ import "./styles.css";
 // That way we don't have to define them all individually
 function Header(props) {
   return (
-    <header className="headerHeader">
-    <h1 className="headerText"> 
-        Our essentials to achieve the perfect style at home.
-    </h1>
-    </header>
+    <div className="jumbotron jumbotron-fluid">
+     {/* <header className="headerHeader"> */}
+     <div className="container">
+     <h1 className="headerText"> Know</h1>
+     <p>We desire for all people to know how to style thier unique brand of beauty</p>
+     <h1 className="headerText" >Grow</h1>
+     <p>We believe our beauty is reflected in adaption, revision and change</p>
+     <h1 className="headerText">Glow</h1>
+     <p>We reflect beauty when we glow from within</p>
+     <h2>Search and Find Your Look Below</h2>
+    {/* </header> */}</div>
+    
+    </div>
+   
   );
 }
 
--- my-app/src/components/navbar/index.js
+++ my-app/src/components/navbar/index.js
@@ -1,25 +1,22 @@
 import React from "react";
 import './styles.css';
 
+
 function Navbar() {
     return (
       <header className="navHeader">
-      <nav className="navbar navbar-inverse">
-      <div>
-      </div>
-      <div className="logo"><a href="/">Know Grow Glow </a></div>
-      <div className="spacer"/>
-        <div className ="navigation_links">
-          <ul>
-            <li>
-            <a href="/" className='nav-link'>Hair Tips</a>
-            </li>
-          </ul>
-        </div>
-      </nav>
+      <div className="logo"><a href="/"> Know Grow Glow</a></div>
+      <ul class="nav justify-content-end">
+      <li class="nav-item">
+      <a class="nav-link active" href="/">Hair Tips</a>
+      <a class="nav-link active" href="/">New Search</a>
+  </li>
+</ul>
       </header>
     );
   }
+
+  
   
   export default Navbar;
   
