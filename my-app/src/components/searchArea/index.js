97e82f12401ab8b03ce8bb13a144a623b9365c91
Janavia Cousins <jlcousins18@gmail.com>
2019-03-14 21:22:14 -0400

    updating search area dropdown

--- my-app/src/components/searchArea/index.js
+++ my-app/src/components/searchArea/index.js
@@ -1,17 +1,42 @@
 import React from "react";
+import Select from 'react-select';
 import './styles.css';
+
+const hairstyles = [
+  { label: "Twistout", value: 1 },
+  { label: "Braidout", value: 2 },
+  { label: "Bantu", value: 3 },
+  { label: "Wash N Go", value: 4 },
+  { label: "Flexi Rods", value: 5 },
+  { label: "Silk Press", value: 6 }
+];
+
+const products = [
+  { label: "Cleanser", value: 1 },
+  { label: "Conditioner", value: 2 },
+  { label: "Moisturizer", value: 3 },
+  { label: "Styling Cream", value: 4 },
+  { label: "Gels", value: 5 },
+  { label: "Oil", value: 6 }
+];
 
 function SearchType() {
     return (
-      <header className="searchHeader">
-      <h1 className="searchText"> Style Your Hair
-          {/* May separate into two components */}
-      </h1>
-      <h1 className="searchText"> Find Your Product
-      </h1>
-      </header>
-    );
+      // <header className="searchHeader">
+      // <h1 className="searchText"> Style Your Hair</h1>
+      // <h1 className="searchText"> Find Your Product</h1>
+      // </header>
+      <div className="container">
+           <div className="row">
+             <div className="col-md-4">
+               <Select options={ hairstyles } />
+             </div>
+             <div className="col-md-4">
+             <Select options={ products } />
+             </div>
+           </div>
+           </div>
+    )
   }
   
   export default SearchType;
-  
